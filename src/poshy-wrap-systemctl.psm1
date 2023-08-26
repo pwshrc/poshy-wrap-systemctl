04eff197f3411a5e2620568ddbc084db7500be4b
Bruce Markham <219281+brucificus@users.noreply.github.com>
2023-08-25 21:56:11 -0400

    ✨ Updates Module Implementation

--- src/poshy-wrap-systemctl.psm1
+++ src/poshy-wrap-systemctl.psm1
@@ -3,7 +3,7 @@ $ErrorActionPreference = "Stop"
 Set-StrictMode -Version Latest
 
 
-if (-not (Test-Command systemctl)) {
+if (-not (Test-Command systemctl) -and (-not $Global:PWSHRC_FORCE_MODULES_EXPORT_UNSUPPORTED)) {
     return
 }
 
